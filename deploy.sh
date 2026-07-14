20da46aa76c110e62c5cdad7ae43464cc7ff2907
L-Gopikrishna <lgopi698@gmail.com>
2026-07-14 21:21:16 +0530

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+echo "Navigate to the project directory..."
+cd "~/sarwa-shopping/"
+
+echo "Fetching latest changes from main..."
+git checkout main
+git fetch --all
+git reset --hard origin/main
+
+echo "Installing dependencies..."
+pnpm install
+
+echo "Restarting API/Main process..."
+pm2 restart 0
+
+echo "Stopping Storefront for build..."
+pm2 stop 2
+
+echo "Building Storefront..."
+cd apps/storefront
+pnpm run build
+
+echo "Restarting Storefront..."
+pm2 restart 2
+
+echo "Deployment completed successfully!"
